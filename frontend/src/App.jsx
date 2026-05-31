136a00761bdeda88e00bf7f7e3fc42d70e126daa
ananyakh16-svg <ananyakh16@gmail.com>
2026-05-31 09:26:59 +0530

    Temporary rename dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/DashboardTemp";
 import Members from "./pages/Members";
 import Events from "./pages/Events";
 import Meetings from "./pages/Meetings";
